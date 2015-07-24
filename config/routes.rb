363ba163611339a3e258e406abe2e0f28f20c596
hirasawa <hirappy.so@gmail.com>
2015-07-24 19:18:25 +0900

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root :to => 'drinks#index'
   resources :materials
   resources :drinks
   # The priority is based upon order of creation: first created -> highest priority.
